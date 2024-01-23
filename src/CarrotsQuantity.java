a5791327a4dd5d9dae39b70561f8cba1af47ba7c
Katerina Hodslavska <hodslavska.k@gmail.com>
2024-01-23 17:05:01 +0100

    New main class in CarrotsQuantity added

--- src/CarrotsQuantity.java
+++ src/CarrotsQuantity.java
@@ -1,2 +1,10 @@
 public class CarrotsQuantity {
+    public static void main(String[] args) {
+        Vendor vendor = new Vendor("Jan", "Novak",1970,10,2, "Prague", 6.0, 4A2380  );
+
+    }
+
+//check the parameters
+
+
 }
