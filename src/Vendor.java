bf1eb59ad8d3584dadc4367bce8ca47192d5d5fa
Katerina Hodslavska <hodslavska.k@gmail.com>
2024-01-23 16:54:45 +0100

    New constructor added

--- src/Vendor.java
+++ src/Vendor.java
@@ -17,4 +17,25 @@ public class Vendor {
     Double vehicleConsumptionPer100Km;
     InetAddress inetAddress;
 
+
+    //constructor
+    public Vendor(String name, String surname, LocalDate DateOfBirth, int numberOfNegotiatedContracts, double quantityTons, String headquarterCity, String vehicleRegistrationPlate, double vehicleConsumptionPer100Km, InetAddress inetAddress)
+    {   this.name = name;
+        this.surname = surname;
+        this.DateOfBirth = DateOfBirth;
+        this.numberOfNegotiatedContracts = numberOfNegotiatedContracts;
+        this.quantityTons = quantityTons;
+        this.headquarterCity = headquarterCity;
+        this.vehicleConsumptionPer100Km = vehicleConsumptionPer100Km;
+        this.vehicleRegistrationPlate = vehicleRegistrationPlate;
+        this.inetAddress = inetAddress;
+
     }
+
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
+}
