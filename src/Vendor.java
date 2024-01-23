31e0b0c9478c2eb578095951b590ddf9c05e956b
Katerina Hodslavska <hodslavska.k@gmail.com>
2024-01-23 14:02:31 +0100

    Variables added

--- src/Vendor.java
+++ src/Vendor.java
@@ -1,6 +1,29 @@
+import java.time.LocalDate;
 
 public class Vendor {
-    public static void main(String[] args) {
+
+
+    String name;
+    String surname;
+    LocalDate DateOfBirth;
+    int numberOfNegotiatedContracts;
+
+    double quantityTons;
+
+    String headquarterCity; //The City of the merchant
+
+    String vehicleRegistrationPlate;
+    Double vehicleConsumptionPer100Km;
+
+
+
+
+
+
+
+
+
+
 
 
     }
